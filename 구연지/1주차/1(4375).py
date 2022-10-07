804b8222a42ebb4b7b1004afacdfb3ff17516efa
Qkite <yeonjigu4@gmail.com>
2022-10-07 13:37:05 +0900

    10.06 업데이트

--- 구연지/1주차/1(4375).py
+++ 구연지/1주차/1(4375).py
@@ -1,1 +1,24 @@
+#  왜 틀렸지...?
+try :
+    input_number = int(input())
+
+    if input_number == 2 or input_number ==5:
+        pass
+    else:
+        find_multiple = False
+        multiple_number = 0
+
+
+        while find_multiple == False:
+            multiple_number = multiple_number*10 +1
+            if multiple_number%input_number == 0:
+                find_multiple = True
+
+        print(str(multiple_number).count('1'))
+except :
+    pass
+
+
+
+
 
