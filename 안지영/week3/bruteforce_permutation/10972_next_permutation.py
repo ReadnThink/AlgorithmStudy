594de4b49fa6c5eecc432ffa1e2ab9e6f7fb979b
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-23 23:59:06 +0900

    10972 다음 순열

---
+++ 안지영/week3/bruteforce_permutation/10972_next_permutation.py
@@ -0,0 +1,34 @@
+# 10972 https://www.acmicpc.net/problem/10972
+# 성공 ! 
+
+def main():
+    
+    N = int(input())
+    
+    arr = list(map(int,input().split()))
+
+    left = []    
+    stop = False
+    
+    while not stop:    
+        
+        if len(arr) == 1:
+            break
+        
+        left.append(arr.pop())
+        
+        if left[-1] > arr[-1]:
+            for i in range(len(left)):
+                if left[i] > arr[-1]:
+                   left[i],arr[-1] = arr[-1], left[i]
+                   break
+            arr = arr + left
+            stop = True
+
+    
+    if stop == False:
+        print('-1')
+    else:
+        print(' '.join(map(str,arr)))
+
+main()
